c32653a38c1c7b66eae0a975f38ffd832409d88c
SharaMulti <jjuulliiooooooooo@gmail.com>
2025-04-30 23:02:22 -0600

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+/* Amerike university
+      Author: Julio López
+      Date: 30/04/25
+      Practice #: 28
+      Description: This program shall use constructors to show data, but this time using a pointer of a pointer*/
+ 
+      #include <iostream>
+      using namespace std;
+ 
+      int main(){
+ 
+         int Number = 4;
+         string Thing = "Hello";
+ 
+         int  *pNumber = &Number;
+         string *pThing = &Thing;
+
+         int **ppNumber = &pNumber;
+         string **ppThing = &pThing;
+
+         cout << *pNumber << "\n";
+         cout << *pThing << "\n"; 
+
+         cout << &Number << "\n";
+         cout << &Number << "\n";
+ 
+         *pNumber = 5;
+         *pThing = "Bye";
+ 
+         cout << "\n";
+ 
+         cout << *pNumber << "\n";
+         cout << *pThing << "\n"; 
+
+         cout << &pNumber << "\n"; 
+         cout << &pNumber << "\n";
+
+         cout << "\n";
+
+         **ppNumber = 7;
+         **ppThing = "Hello again";
+
+         cout << **ppNumber << "\n";
+         cout << **ppThing << "\n";
+
+         cout << &ppNumber << "\n";
+         cout << &ppNumber << "\n";
+ 
+ 
+ 
+ 
+ 
+ 
+      }
